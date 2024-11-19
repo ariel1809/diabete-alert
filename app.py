c5534c98284c3a3e4c1dbe0b79f8a3a95a806bc0
ariel1809 <stevekamga18@gmail.com>
2024-11-19 15:55:58 +0100

    static

--- app.py
+++ app.py
@@ -1,41 +1,4 @@
-from flask import Flask, jsonify
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-# Charger les variables d'environnement depuis le fichier .env
-load_dotenv()
-
-app = Flask(__name__)
-
-# Charger les variables d'environnement pour l'URI de MongoDB
-# MONGO_USER = os.getenv("MONGO_USER")
-# MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_HOST = os.getenv("MONGO_HOST")
-MONGO_PORT = os.getenv("MONGO_PORT")
-MONGO_DB = os.getenv("MONGO_DB")
-
-# Encoder le mot de passe si nécessaire
-# MONGO_PASSWORD = MONGO_PASSWORD.replace("#", "%23")  # Exemple pour encoder '#'
-
-# Construire l'URI MongoDB
-MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
-print(MONGO_URI)
-
-# Initialiser le client MongoDB
-client = MongoClient(MONGO_URI)
-
-# Accéder à la base de données
-db = client.get_database()
-
-@app.route('/test-connexion')
-def test_connexion():
-    try:
-        # Tester la connexion en listant les collections
-        collections = db.list_collection_names()
-        return jsonify({"success": True, "collections": collections}), 200
-    except Exception as e:
-        return jsonify({"success": False, "error": str(e)}), 500
+from dashboar import app
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ dashboar/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify, render_template
+
+
+# Créer une instance de l'application Flask
+app = Flask(__name__)
+
+# Route pour la page du chatbot
+@app.route('/login')
+def login():
+    return render_template('dashboard/authentication-login.html')
+
+@app.route('/register')
+def register():
+    return render_template('dashboard/authentication-register.html')
