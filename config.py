79825633cdb70bc33dace64a1cb805d276601cf7
ariel1809 <stevekamga18@gmail.com>
2024-12-08 14:23:51 +0100

    render database

--- config.py
+++ config.py
@@ -4,8 +4,12 @@ from dotenv import load_dotenv
 # Charger les variables d'environnement
 load_dotenv()
 
+
 class Config:
-    MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
-    MONGO_PORT = os.getenv("MONGO_PORT", 27017)
+    MONGO_USER = os.getenv("MONGO_USER", "arielo")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "Licence1234")
+    MONGO_HOST = os.getenv("MONGO_HOST", "cluster0.0qgzg.mongodb.net")
     MONGO_DB = os.getenv("MONGO_DB", "diabeteAlertDB")
-    MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}"
+
+    # URI MongoDB corrigé pour MongoDB Atlas
+    MONGO_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_DB}?retryWrites=true&w=majority"
