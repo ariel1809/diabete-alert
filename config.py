0ae6cc0fc8e025bdb8d7932a16facc8bbd756a8c
ariel1809 <stevekamga18@gmail.com>
2024-12-12 14:39:24 +0100

    env

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 
 class Config:
-    MONGO_USER = os.getenv("MONGO_USER", "arielo")
-    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "Licence1234")
-    MONGO_HOST = os.getenv("MONGO_HOST", "cluster0.0qgzg.mongodb.net")
+    MONGO_USER = os.getenv("MONGO_USER", "arielo18")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "Azerty18")
+    MONGO_HOST = os.getenv("MONGO_HOST", "cluster0.ousup.mongodb.net")
     MONGO_DB = os.getenv("MONGO_DB", "diabeteAlertDB")
 
     # URI MongoDB corrigé pour MongoDB Atlas
-    MONGO_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_DB}?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+    MONGO_URI = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_DB}"
